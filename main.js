52e6a8065f0ca97b6c3f1491eaafb2d1b93548e9
Entaro Adun <dcwmark@gmail.com>
2020-02-10 08:46:34 -0500

    Added proof of work

--- main.js
+++ main.js
@@ -5,22 +5,33 @@ class Block {
      * @param {number} timestamp 
      * @param {*} data 
      * @param {string} previousHash 
-     */
+    **/
     constructor(timestamp, data, previousHash = '') {
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(`${this.previousHash}${this.timestamp}${JSON.stringify(this.data)}`).toString();
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) != Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log(`Block mind: {this.hash}`);
     }
 }
 
 class BlockChain {
     constructor() {
         this.chain = [this.createGensisBlock()];
+        this.difficulty = 2;
     }
 
     createGensisBlock() {
@@ -33,7 +44,7 @@ class BlockChain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLastestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -56,7 +67,17 @@ class BlockChain {
 }
 
 let naiveCoin = new BlockChain();
+
+let start = Date.now();
+console.log(`Mining block 1 ... ${start}`);
 naiveCoin.addBlock(new Block("10/12/2020", { amount: 10 }));
+console.log(`Done block 1 ... ${Date.now() - start} ms`);
+console.log('');
+
+start = Date.now();
+console.log(`Mining block 2 ... ${start}`);
 naiveCoin.addBlock(new Block("12/10/2020", { amount: 50 }));
+console.log(`Done block 2 ... ${Date.now() - start} ms`);
+console.log('');
 
 console.log(JSON.stringify(naiveCoin, null, 2));
